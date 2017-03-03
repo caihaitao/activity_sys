a93d75d0e0d991484dad60d3e3be340c03ccde35
caihaitao <caihaitao119@163.com>
2017-03-03 18:32:03 +0800

    初始化脚本

---
+++ src/main/resources/mapper/init.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `t_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `activityName` varchar(50) DEFAULT NULL,
+  `imagePath` varchar(200) DEFAULT NULL,
+  `remark` varchar(150) DEFAULT NULL,
+  `activityDate` date DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  `cost` double(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_activity_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `activityId` int(11) NOT NULL,
+  `activityName` varchar(100) DEFAULT NULL,
+  `activityDate` date DEFAULT NULL,
+  `playerId` int(11) NOT NULL,
+  `playerName` varchar(50) DEFAULT NULL,
+  `recordTime` datetime DEFAULT NULL,
+  `playerMobile` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique` (`activityId`,`playerId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_player` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(50) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `imagePath` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `imagePath` varchar(200) DEFAULT NULL,
+  `realname` varchar(50) DEFAULT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `un` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
